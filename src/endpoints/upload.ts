d6c586cec9d607fcf36ff945b815dbcb9224925c
rodrigosuelli <rodrigosuelli@gmail.com>
2024-08-27 20:24:14 -0300

    Small changes to upload endpoint

--- src/endpoints/upload.ts
+++ src/endpoints/upload.ts
@@ -1,7 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 
+type UploadReqBody = {
+  image?: string;
+  customer_code?: string;
+  measure_datetime?: string;
+  measure_type?: string;
+};
+
 export function upload(req: Request, res: Response, next: NextFunction) {
   try {
+    const { image, customer_code, measure_datetime, measure_type } =
+      req.body as UploadReqBody;
+
+    console.log(req.body);
+
+    if (measure_type !== 'WATER' && measure_type !== 'GAS') {
+      throw new Error(
+        'Error: Measure type needs to be equals to `WATER` or `GAS`'
+      );
+    }
+
     res.status(200).json({ success: true });
   } catch (error) {
     next(error);
