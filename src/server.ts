16d3f1cc7cf37d215027de6324710821ecf617ba
rodrigosuelli <rodrigosuelli@gmail.com>
2024-08-28 15:43:04 -0300

    Increase limit to 2mb

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json()); // required to parse json body from incoming requests
+app.use(express.json({ limit: '2mb' })); // required to parse json body from incoming requests
 
 app.use('/', router);
 
