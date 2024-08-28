89920a1f4f0156503523297a74e0352deafaf849
rodrigosuelli <rodrigosuelli@gmail.com>
2024-08-28 15:59:30 -0300

    Use gemini-1.5-flash

--- src/services/geminiApi.ts
+++ src/services/geminiApi.ts
@@ -13,7 +13,7 @@ const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
   // Choose a Gemini model.
-  model: 'gemini-1.5-pro',
+  model: 'gemini-1.5-flash',
 });
 
 export async function promptWithBase64Image(
