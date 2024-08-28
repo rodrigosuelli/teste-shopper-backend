9280aa442c9ce9a390fe42692f8a1c61e3c717c7
rodrigosuelli <rodrigosuelli@gmail.com>
2024-08-28 15:22:03 -0300

    Prompt with local image intead of upload

--- src/endpoints/upload.ts
+++ src/endpoints/upload.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 import { fromError } from 'zod-validation-error';
+import { promptWithBase64Image } from '../services/geminiApi';
 
-export function upload(req: Request, res: Response, next: NextFunction) {
+export async function upload(req: Request, res: Response, next: NextFunction) {
   try {
     const uploadEndpointSchema = z.object({
       image: z.string().base64(),
@@ -12,6 +13,14 @@ export function upload(req: Request, res: Response, next: NextFunction) {
     });
 
     const data = uploadEndpointSchema.parse(req.body);
+
+    const { image: imageBase64String } = data;
+
+    const result = await promptWithBase64Image(
+      'image/png',
+      imageBase64String,
+      'Describe the image'
+    );
 
     res.status(200).json({ success: true, ...data });
   } catch (error) {
--- src/services/geminiApi.ts
+++ src/services/geminiApi.ts
@@ -1,8 +1,4 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import {
-  GoogleAIFileManager,
-  UploadFileResponse,
-} from '@google/generative-ai/server';
 
 const { GEMINI_API_KEY } = process.env;
 
@@ -14,45 +10,26 @@ if (!GEMINI_API_KEY) {
 
 // Access your API key as an environment variable
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const fileManager = new GoogleAIFileManager(GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
   // Choose a Gemini model.
   model: 'gemini-1.5-flash',
 });
 
-export async function uploadImageFile(
-  filePathWithExtension: string,
+export async function promptWithBase64Image(
   mimeType: string,
-  displayName: string
-) {
-  // Upload the file and specify a display name.
-  const uploadResponse = await fileManager.uploadFile(filePathWithExtension, {
-    mimeType,
-    displayName,
-  });
-
-  // View the response.
-  console.log(
-    `Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`
-  );
-
-  return uploadResponse;
-}
-
-export async function promptWithUploadedImage(
-  uploadFileResponse: UploadFileResponse,
+  base64String: string,
   promptText: string
 ) {
   // Generate content using text and the URI reference for the uploaded file.
   const result = await model.generateContent([
+    { text: promptText },
     {
-      fileData: {
-        mimeType: uploadFileResponse.file.mimeType,
-        fileUri: uploadFileResponse.file.uri,
+      inlineData: {
+        data: base64String,
+        mimeType,
       },
     },
-    { text: promptText },
   ]);
 
   // Output the generated text to the console
